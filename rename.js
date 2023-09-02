a3fe474692d04cf739e3ffc04a517b1d4ce05690
Usiel Piña <alonso722243@gmail.com>
2023-09-02 14:26:47 -0600

    Ajuste reame js

--- rename.js
+++ rename.js
@@ -12,9 +12,18 @@ function getCurrentFilenames() {
     if(element[1]){
         if(element[1]==="jpg")
         {
-            fs.rename(element[0]+'.'+element[1], 'arma_'+contador+'.jpg', () => {
-                console.log("\nFile Renamed!:" + contador);
-            });
+            console.log("Haciendo rename")
+            // fs.rename(element[0]+'.'+element[1], 'arma_'+contador+'.jpg', () => {
+            //     console.log("\nFile Renamed!:" + contador);
+            // });
+            fs.copyFile(element[0]+'.'+element[1], 'arma_'+contador+'.jpg', (err) => {
+                if (err) {
+                  console.log("Error Found:", err);
+                }
+                else {
+                  console.log("\nFile Contents ")
+               }
+              });
             contador++;
         
         }
