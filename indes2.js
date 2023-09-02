472c0e8e629b53386e060663d249a984fabbdc02
Usiel Piña <alonso722243@gmail.com>
2023-09-02 13:57:50 -0600

    Add rename function

---
+++ indes2.js
@@ -0,0 +1,24 @@
+// fs.rename() method 
+     
+// Import filesystem module
+const fs = require('fs');
+   
+// List all the filenames before renaming
+getCurrentFilenames();
+   
+// Rename the file
+fs.rename('hola.txt', 'world.txt', () => {
+  console.log("\nFile Renamed!\n");
+   
+  // List all the filenames after renaming
+  getCurrentFilenames();
+});
+   
+// Function to get current filenames
+// in directory
+function getCurrentFilenames() {
+  console.log("Current filenames:");
+  fs.readdirSync(__dirname).forEach(file => {
+    console.log(file);
+  });
+}
